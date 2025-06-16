af3f86710bd235ed9ee54c81ea7e007aef0f2233
v-gedipudi1 <78712082+v-gedipudi1@users.noreply.github.com>
2025-06-16 10:54:18 -0400

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDz_QSjc9_pWWZSc1oQfcxuM6VWJRzNvPI",
+  authDomain: "patient-portal-app-48bce.firebaseapp.com",
+  projectId: "patient-portal-app-48bce",
+  storageBucket: "patient-portal-app-48bce.firebasestorage.app",
+  messagingSenderId: "36675680327",
+  appId: "1:36675680327:web:d38c1bbc7cb1f600b85962"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
